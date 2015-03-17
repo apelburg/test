d9c63e5c777152b43a07438270fc06f768b089a1
adreew <premier22@yandex.ru>
2015-03-17 18:58:21 +0600

    поменял структуру

---
+++ modules/clients/client_folder/business_offers/to_print.php
@@ -0,0 +1,28 @@
+<?php 
+    header('Content-type: text/html; charset=utf-8');
+	error_reporting(E_ALL);
+	
+	include_once($_SERVER['DOCUMENT_ROOT']."/os/libs/php/classes/art_img_class.php");
+	include_once($_SERVER['DOCUMENT_ROOT']."/os/libs/php/classes/com_pred_class.php");
+?>
+<script type="text/javascript">
+    window.onload(window.print());
+</script>
+
+<?php	
+    //echo $_SERVER['DOCUMENT_ROOT'].'/os/libs/php/common.php';
+	
+    include($_SERVER['DOCUMENT_ROOT'].'/os/libs/php/common.php');
+	//echo; 
+	list($version,$param) = explode('{@}',$_GET['data']);
+	if($version == 'new'){
+	    include($_SERVER['DOCUMENT_ROOT'].'/os/libs/mysqli.php');
+	    include($_SERVER['DOCUMENT_ROOT'].'/os/libs/config.php');
+	    
+	    list($id,$client_id,$manager_id) = explode('-',$param);
+	    echo Com_pred::open_in_blank($id,$client_id,$manager_id);
+	}
+	if($version == 'old') Com_pred::open_old_kp($param);
+	exit;
+	
+?>
