93fcc4f56a8d2cc56fb45d4a40027eb804e6fad3
adreew <premier22@yandex.ru>
2015-03-19 13:34:55 +0600

    --

--- libs/php/classes/manager_class.php
+++ libs/php/classes/manager_class.php
@@ -21,18 +21,6 @@ class Manager {
 	        // обработка пустой выборки
 	    }
 	}	
-	public function get_mail_signature($id){
-		
-		   global $mysqli;
-		   
-		   $query="SELECT*FROM `".MANAGERS_TBL."`  WHERE `id` = '".(int)$id."'";
-		   $result = $mysqli->query($query)or die($mysqli->error);
-		   if($result->num_rows>0){
-		       $manager_data=$result->fetch_assoc();
-		   }
-		
-		return convert_bb_tags($manager_data['mail_signature']);
-	}	
 }
 
 ?>
