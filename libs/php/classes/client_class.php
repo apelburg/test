a79e41afdc776d29aafb0cb141178fd4eeff8d06
Alexey <kapitonoval@gmail.com>
2015-03-19 17:15:42 +0300

    добавил описание класса

--- libs/php/classes/client_class.php
+++ libs/php/classes/client_class.php
@@ -1,12 +1,103 @@
 ﻿<?php
 class Client {	
+	#################################
+	###         СВОЙСТВА          ###
+	#################################
+	// содержит название и пути к изображениям для каждого типа контактных данных
 	static $array_img = array('email'=>'<img src="skins/images/img_design/social_icon1.png" >','skype' => '<img src="skins/images/img_design/social_icon2.png" >','isq' => '<img src="skins/images/img_design/social_icon3.png" >','twitter' => '<img src="skins/images/img_design/social_icon4.png" >','fb' => '<img src="skins/images/img_design/social_icon5.png" >',
 'vk' => '<img src="skins/images/img_design/social_icon6.png" >','other' => '<img src="skins/images/img_design/social_icon7.png" >');
+
+	#################################
+	###          МЕТОДЫ           ###
+	#################################
+	/*
+	__construct($id) 
+	(id клиента , массив)
+	// получает основные данные о клиенте 
 	
+	public function create($array_in)
+	(массив с ключами в виде названий колонок основной таблицы клиентов)
+	// создаёт клиента
+
+
+	static function delete($id)
+	(id клиента)
+	// удалить клиента и всё, что с ним связано
+	// возвращает строку сообщене об успешном удалении
+	
+
+	get_addres($id)  
+	(id клиента , массив)
+	//получает адрес доставки и фактический адрес клиента
+	
+	get_requisites($client_id) 
+	(id клиента)
+	// получает массив реквизитов 
+
+	get_contact_info_arr($tbl,$type,$parent_id)
+	(имя константы с названием таблицы, фильтр по типу (если не указан выводит все), 
+	id строки родителя из той таблицы по которой выполняется поиск)
+	// получаем контактные данные только по клиенту
+	
+	get_contats_info_all($type, $client_id)
+	(фильтр по типу (если не указан выводит все), 
+	id строки родителя из той таблицы по которой выполняется поиск)
+	// получаем контактные данные по клиенту и его контактным лицам
+	
+	requisites($id)
+	(id клиента) int
+	// получаем массив реквизитов
+	
+	cont_faces($id)
+	(id клиента) int
+	//получаем данные о контактных дицах компании
+
+	get_relate_managers($id)
+	(id клиента)
+	// получаем кураторов клиента в массиве
+	
+	private function get_manager_info_by_id($manager_id)
+	//Получение данных о Менеджере по id
+	//возвращает одномерный массив
+
+	function cor_data_for_SQL($data)
+	//защита от sql инъекций
+	
+	######################################################
+	###   НЕ УНИФИЦИРОВАННЫЕ (УЗКОНАПРАВЛЕННЫЕ) МЕТОДЫ ###
+	######################################################
+
+	get_contact_row($contact_company, $type,$array_dop_contacts_img) 
+	(массив контактов,строка тип, массив с изображениями)
+	// вывод доп контактов в табличном виде,
+	
+	get__clients_persons_for_requisites($type)
+	(id уже выбранной строки, по умолчанию необходимо посылать 0)
+	// отдает список <option> с названиями занесенных в базу должностей сотрудников для реквизитов
+	
+	edit_requsits_show_person($requisites_id)
+	(id реквизита)
+	// лица (контрагенты) имеющие право подписи  для реквизитов в массиве
+
+	edit_requsits_show_person_all($arr,$client_id)
+	(массив персон имеющих право подписи, id клиента)
+	// выводит html форму редактирования персон имеющих право подписи
+
+	get_relate_managers($client_id)
+	(id клиента)
+	// получаем кураторов клиента через запятую
+
+	get_reiting($id,$rate)
+	(id клиента, число рейтинга от 0-5)
+	// вывод html рейтинга
+	
+	get_contact_info($tbl,$parent_id)
+	(константа таблицы, id строки родителя')
+	// выовд контактных данных в html
+
+	*/
 	public function __construct($id) {
-		global $mysqli;
-		
-		//$this->id = $id;	
+		global $mysqli;		
 		//получаем данные из основной таблицы
 		$query = "SELECT * FROM `".CLIENTS_TBL."` WHERE `id` = '".(int)$id."'";
 		$result = $mysqli->query($query) or die($mysqli->error);
@@ -16,16 +107,14 @@ class Client {
 				$this->name = $row['company'];
 			}
 		}
-
 		//получаем телефоны, email, vk и т.д.	
 		$arr = $this->get_contact_info("CLIENTS_TBL",$id);
 		$this->cont_company_phone = (isset($arr['phone']))?$arr['phone']:''; 
 		$this->cont_company_other = (isset($arr['other']))?$arr['other']:'';
 	}
-
 	static function get_addres($id){
 		global $mysqli;
-		$query = "SELECT * FROM  `order_manager__client_addres_tbl` WHERE `parent_id` = '".(int)$id."'";
+		$query = "SELECT * FROM  `".CLIENT_ADRES_TBL."` WHERE `parent_id` = '".(int)$id."'";
 		$arr = array();
 		$result = $mysqli->query($query) or die($mysqli->error);
 		if($result->num_rows > 0){
@@ -35,8 +124,6 @@ class Client {
 		}
 		return $arr;
 	}
-
-	//вывод доп контактов в табличном виде
 	static function get_contact_row($contact_company, $type,$array_dop_contacts_img){
 		
 		if(isset($type) && $type == "phone"){
@@ -71,8 +158,6 @@ class Client {
 		}			
 	}
 	static function get__clients_persons_for_requisites($type){
-		// string
-		// отдает список <option> с названиями занесенных в базу должностей сотрудников для реквизитов
 		global $mysqli;
 		$query = "SELECT * FROM `".CLIENT_PERSON_REQ_TBL."`";
 		$str = "<option value=\"0\">Выберите должность...</option>".PHP_EOL;
@@ -84,15 +169,12 @@ class Client {
 		}
 		return $str;
 	} 
-
-	static function edit_requsits_show_person($person_id){
+	static function edit_requsits_show_person($requisites_id){
 		// array
-		// отдаёт контакты для реквизитов в массиве
+		// лица (контрагенты) имеющие право подписи  для реквизитов в массиве
 		global $mysqli;
-		$arr = "";
-
-		// $query = "SELECT * FROM `".CLIENT_REQUISITES_MANAGMENT_FACES_TBL."` INNER JOIN   `".CLIENT_PERSON_REQ_TBL."` ON   `".CLIENT_PERSON_REQ_TBL."`.`id` = `".CLIENT_REQUISITES_MANAGMENT_FACES_TBL."`.`post_id` WHERE `requisites_id` = '".$person_id."'";
-		$query = "SELECT * FROM `".CLIENT_REQUISITES_MANAGMENT_FACES_TBL."` WHERE `requisites_id` = '".$person_id."'";
+		$arr = array();
+		$query = "SELECT * FROM `".CLIENT_REQUISITES_MANAGMENT_FACES_TBL."` WHERE `requisites_id` = '".$requisites_id."'";
 		$result = $mysqli->query($query) or die($mysqli->error);
 		if($result->num_rows > 0){
 			while($row = $result->fetch_assoc()){
@@ -101,42 +183,12 @@ class Client {
 		}
 		return $arr;
 	}
-
 	static function edit_requsits_show_person_all($arr,$client_id){
-		// string (html)
-		// обрабатывает массив контактных данных из реквизитов и создаёт html
-		// ob_start();
-
-		// получаем список должностей для персональных данных контактных лиц из реквизитов
-		
-		
-		// echo "<pre>";
-		// print_r($arr);
-		// echo "</pre>";
 		foreach ($arr as $key => $contact) {
 			$get__clients_persons_for_requisites = Client::get__clients_persons_for_requisites($contact['post_id']);
 			include('./skins/tpl/clients/client_folder/client_card/edit_requsits_show_person.tpl');
 		}
-		// $html = ob_get_contents();
-		// ob_get_clean();
-
-		// return $html;
 	}
-
-	static function get_relate_managers($client_id){
-		global $mysqli;
-		$query = "SELECT * FROM  `".MANAGERS_TBL."` WHERE `id` IN (SELECT `manager_id` FROM  `".RELATE_CLIENT_MANAGER_TBL."`  WHERE `client_id` IN (SELECT `id` FROM `".CLIENTS_TBL."` WHERE `id` = 1894 ));";
-		$result = $mysqli->query($query) or die($mysqli->error);
-		$manager_names = "";				
-		if($result->num_rows > 0){
-			while($row = $result->fetch_assoc()){
-				if($manager_names!=""){$manager_names .=", ";}
-				$manager_names .= $row['name'];
-			}
-		}
-		return $manager_names;
-	}
-
 	static function get_requisites($client_id){
 		global $mysqli;
 		$query = "SELECT * FROM `".CLIENT_REQUISITES_TBL."` WHERE `client_id` = '".$client_id."'";
@@ -149,7 +201,6 @@ class Client {
 		}
 		return $requisites;
 	}
-
 	static function get_reiting($id,$rate){
 		$arr[0] = array('5','0');
 		$arr[1] = array('5','5');
@@ -164,7 +215,6 @@ class Client {
 		</div>';
 		return $r;
 	}
-
 	public function get_contact_info_arr($tbl,$type,$parent_id){
 		global $mysqli;
 		$contacts = array();
@@ -178,7 +228,6 @@ class Client {
 		}
 		return $contacts;
 	}
-
 	static function get_contats_info_all($type, $client_id){
 		// получаем данные по клиенту
 		$contacts = self::get_contact_info_arr('CLIENTS_TBL',$type,$client_id);
@@ -195,9 +244,7 @@ class Client {
 			}
 		}
 		return $contacts;
-
-	}
-	
+	}	
 	public function get_contact_info($tbl,$parent_id){
 		global $mysqli;
 		$query = "SELECT * FROM `".CLIENT_CONT_FACES_CONTACT_INFO_TBL."` WHERE `table` = '".$tbl."' AND `parent_id` = '".$parent_id."'";
@@ -216,9 +263,7 @@ class Client {
 			$contact['other'] = self::get_contact_row($contacts, 'other',self::$array_img);
 		}
 		return $contact;
-
 	}
-	//получаем реквизиты компании
 	static function requisites($id) {
 		global $mysqli;
 		$query = "SELECT * FROM `".CLIENT_REQUISITES_TBL."` WHERE `client_id` = '".(int)$id."'";
@@ -232,7 +277,6 @@ class Client {
 		}
 		return $array;
 	}
-	//получаем данные о контактных дицах компании
 	static function cont_faces($id){
 		global $mysqli;
 		$query = "SELECT * FROM `".CLIENT_CONT_FACES_TBL."` WHERE `client_id` = '".(int)$id."'";
@@ -252,7 +296,6 @@ class Client {
 		
 		return $array;
 	}
-	//получаем данные о прикреплённых менеджерах
 	static function relate_managers($id){
 		global $mysqli;
 		$query = "SELECT * FROM `".MANAGERS_TBL."` INNER JOIN `".RELATE_CLIENT_MANAGER_TBL."` ON `".RELATE_CLIENT_MANAGER_TBL."`.`manager_id` = `".MANAGERS_TBL."`.`id` WHERE `".RELATE_CLIENT_MANAGER_TBL."`.`client_id` = '".(int)$id."'";
@@ -266,7 +309,19 @@ class Client {
 		}
 		return $array;
 	}	
-	//удалить клиента и всё, что с ним связано
+	static function get_relate_managers($client_id){
+		global $mysqli;
+		$query = "SELECT * FROM  `".MANAGERS_TBL."` WHERE `id` IN (SELECT `manager_id` FROM  `".RELATE_CLIENT_MANAGER_TBL."`  WHERE `client_id` IN (SELECT `id` FROM `".CLIENTS_TBL."` WHERE `id` = ".$client_id." ));";
+		$result = $mysqli->query($query) or die($mysqli->error);
+		$manager_names = "";				
+		if($result->num_rows > 0){
+			while($row = $result->fetch_assoc()){
+				if($manager_names!=""){$manager_names .=", ";}
+				$manager_names .= $row['name'];
+			}
+		}
+		return $manager_names;
+	}
 	static function delete($id){
 		global $mysqli;
 		//выполняем все запросы ипишем ОК
@@ -301,9 +356,7 @@ class Client {
 		//return $query;
 		$result = $mysqli->multi_query($query) or die($mysqli->error);
 		return "Клиент id ".$id." успешно удален.";		
-	}
-	
-	//заводим клиента
+	}	
 	public function create($array_in){
 		if(empty($array_in))return "не достаточно данных";
 		global $mysqli;
@@ -354,17 +407,13 @@ class Client {
 		//$mail->sendMail(2,'apelburg.m7@gmail.com','Новый клиент',$message,$headers);		
 		$mail->sendMail(2,'kapitonoval2012@gmail.com','Новый клиент',$message,$headers);		
 		return $client_id;
-	}
-	
-	//защита от sql инъекций
+	}		
 	private function cor_data_for_SQL($data){
 	    if(is_int($data) || is_double($data)) return($data);
 	    //return strtr($data,"1","2");
 		$data = strip_tags($data,'<b><br><a>');
 		return mysql_real_escape_string($data);
 	}
-	//Получение данных о Менеджере по id
-	//возвращает одномерный массив
 	private function get_manager_info_by_id($manager_id){
 		global $mysqli;
 		$query = "SELECT * FROM `".MANAGERS_TBL."` WHERE `id` = '".$manager_id."'";
