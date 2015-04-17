84bd319800eef30adeca27d04a2eb7217ec21cfb
adreew <premier22@yandex.ru>
2015-04-17 20:27:35 +0600

    авторизация

--- modules/samples/router.php
+++ modules/samples/router.php
@@ -2,8 +2,8 @@
     
 	// ** БЕЗОПАСНОСТЬ **
 	// проверяем выдан ли доступ на вход на эту страницу
-	// если нет $ACCESS['suppliers']['access'] или она равна FALSE прерываем работу скирпта 
-	if(!@$ACCESS['suppliers']['access']) exit($ACCESS_NOTICE);
+	// если нет $ACCESS['название раздела']['access'] или она равна FALSE прерываем работу скирпта 
+	if(!@$ACCESS['samples']['access']) exit($ACCESS_NOTICE);
 	// ** БЕЗОПАСНОСТЬ **
 
 include 'functions_samples.php';
