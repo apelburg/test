1aeccda846de9fbf432698feacb499a8a965932e
adreew <premier22@yandex.ru>
2015-04-17 19:27:09 +0600

    авторизация

--- modules/clients/router.php
+++ modules/clients/router.php
@@ -1,4 +1,10 @@
 <?php
+
+    // ** БЕЗОПАСНОСТЬ **
+	// проверяем выдан ли доступ на вход на эту страницу
+	// если нет $ACCESS['suppliers']['access'] или она равна FALSE прерываем работу скирпта 
+	if(!@$ACCESS['suppliers']['access']) exit($ACCESS_NOTICE);
+	// ** БЕЗОПАСНОСТЬ **
 	
      include_once('./libs/php/classes/client_class.php');
 	 
--- modules/samples/router.php
+++ modules/samples/router.php
@@ -1,4 +1,11 @@
 <?php
+    
+	// ** БЕЗОПАСНОСТЬ **
+	// проверяем выдан ли доступ на вход на эту страницу
+	// если нет $ACCESS['suppliers']['access'] или она равна FALSE прерываем работу скирпта 
+	if(!@$ACCESS['suppliers']['access']) exit($ACCESS_NOTICE);
+	// ** БЕЗОПАСНОСТЬ **
+
 include 'functions_samples.php';
 ?>
 <div id="sample">
