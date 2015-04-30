90ad7b0644ff1f8c82d8475631c1dd7dbbd4fd90
Alexey <kapitonoval@gmail.com>
2015-04-30 14:09:11 +0300

    перенос в client_folder

---
+++ modules/client_folder/order_art_edit/router.php
@@ -0,0 +1,15 @@
+<?php
+
+    // ** БЕЗОПАСНОСТЬ **
+	// проверяем выдан ли доступ на вход на эту страницу
+	// если нет $ACCESS['suppliers']['access'] или она равна FALSE прерываем работу скирпта 
+	if(!@$ACCESS['client_folder']['section']['order_art_edit']['access']) exit($ACCESS_NOTICE);
+	// ** БЕЗОПАСНОСТЬ **
+	
+	// шаблон поиска
+	include'./skins/tpl/common/quick_bar.tpl';
+
+
+	// шаблон страницы
+	include 'skins/tpl/client_folder/order_art_edit/show.tpl';
+	
---
+++ skins/tpl/client_folder/order_art_edit/show.tpl
@@ -0,0 +1,9 @@
+<link href="./skins/css/order_art_edit.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="libs/js/order_art_edit.js"></script>
+
+<div id="info_string_on_query">
+	<span id="back_to_string_of_claim"></span>
+	<span id="claim_number"></span>
+	<span id="button_standart_001"></span>
+	
+</div>
