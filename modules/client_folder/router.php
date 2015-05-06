f4ceea72db63ed1f7e9fbab66c96bc9270cf2514
adreew <premier22@yandex.ru>
2015-05-06 17:17:29 +0600

    --

---
+++ modules/client_folder/router.php
@@ -0,0 +1,34 @@
+<?php
+
+    // ** БЕЗОПАСНОСТЬ **
+	// проверяем выдан ли доступ на вход на эту страницу
+	// если нет $ACCESS['название раздела']['access'] или она равна FALSE прерываем работу скирпта 
+	if(!@$ACCESS['client_folder']['access']) exit($ACCESS_NOTICE);
+	// ** БЕЗОПАСНОСТЬ **
+	
+	
+	ob_start();	
+	 
+	 switch($section){
+	 
+	   case 'rt':
+	   include 'rt/router.php';
+	   break;
+
+	   case 'order_art_edit':
+	   include 'order_art_edit/router.php';
+	   break;
+
+	   default: 
+	   include 'rt/router.php';
+	   break;
+	
+	}
+	
+	$content = ob_get_contents();
+	ob_get_clean();
+
+	include'./skins/tpl/clients/show.tpl';
+	
+	unset($content);
+?>
