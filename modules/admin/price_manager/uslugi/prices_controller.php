cbfc3b2584596308d47af217f08945c36672dd78
adreew <premier22@yandex.ru>
2015-06-19 12:32:43 +0600

    прайс менеджер

--- modules/admin/price_manager/uslugi/prices_controller.php
+++ modules/admin/price_manager/uslugi/prices_controller.php
@@ -1,36 +1,125 @@
 <?php 
 
+    if(!empty($_POST['dataBufferForSavingToBase'])){
+	    
+	    $data = json_decode($_POST['dataBufferForSavingToBase']);
+	    //echo '<pre>'; print_r($data);echo '</pre>';print_r($data);//
+		//exit;
+		
+		$query ="DELETE FROM `".BASE__CALCULATORS_PRICE_TABLES_TBL."` WHERE 
+		                            `print_type_id` = '".$data->print_type_id."' AND
+									`price_type` = '".$data->price_type."' AND
+									`count` = '".$data->count."'";
+		echo $query;echo '<br>';
+		$mysqli->query($query)or die($mysqli->error);
+		
+		foreach($data->tbl_data as $val){
+		    $query ="INSERT INTO `".BASE__CALCULATORS_PRICE_TABLES_TBL."` VALUES('',";
+			$query.= "'".$data->print_type_id."','".$data->price_type."','".$data->count."'";
+			
+			
+			for($i=0;$i<=21;$i++){
+			   if(isset($val[$i])) $query.= ",'".$val[$i]."'";
+			   else $query.= ",''";
+		    }
+            $query.= ")";
+			
+	        //echo $query;echo '<br>';
+	        $result = $mysqli->query($query)or die($mysqli->error);
+		}
+		
+		header('location:'.$_SERVER['HTTP_REFERER']);
+		
+	}
+
     $usluga_id = $_GET['usluga'];
 	
-	$td1  = '<td>'; 
+	$td1  = '<td contenteditable="true">'; 
 	$td2  = '</td>';
 	$td_td  = $td2.$td1;
 	$tr1  = '<tr>'; 
 	$tr2  = '</tr>';
 	$tr_tr  = $tr2.$tr1;
 	$tbl_types = array();
-	// выбираем данные из таблицу содержащей прайсы
-	$query="SELECT*FROM `".BASE__CALCULATORS_PRICE_TABLES_TBL."` WHERE `print_type_id` = '".$usluga_id."' ORDER by id, param_val";
+	// выбираем данные из таблицы содержащей прайсы
+	$query="SELECT*FROM `".BASE__CALCULATORS_PRICE_TABLES_TBL."` WHERE `print_type_id` = '".$usluga_id."' ORDER by price_type, id, param_val";
 	//echo $query;
 	$result = $mysqli->query($query)or die($mysqli->error);
+	
 	if($result->num_rows>0){
-		while($row = $result->fetch_assoc()){
+		while($row = $result->fetch_assoc()){ 
+		     
 		     $price_type=$row['price_type'];
+			 $count=$row['count'];
 		     unset($row['id'],$row['print_type_id'],$row['count'],$row['price_type']);
+			 
+			 if(!isset($end[$price_type]))$end[$price_type] = false;
 			 // создаем массив содержащий вариации существующих в таблице прайсов
-		     if(!in_array($price_type,$tbl_types)) $tbl_types[] = $price_type;
-		     $tbl_row[$price_type][] = $tr1.$td1.implode($td_td,$row).$td2.$tr2;
+		     if(!in_array($price_type,$tbl_types)) $tbl_types[$price_type]['count'] = $count;
+			 
+			 
+			 
+			 //
+			 if($row['param_val']==0){
+			      array_walk($row, function(&$val, $key){ if($key!='param_type')$val = (int)$val;});
+				  
+				 
+				  $counter = 0;
+				  foreach($row as $key => $val){
+					  if($key!='param_val' && $key!='param_type'){
+					      //echo $val;
+						  if($val==0 && !$end[$price_type]) $end[$price_type] = $counter;
+					  } 
+					  $counter++;
+				  }
+			 }
+			 //echo $end;
+			 if($end[$price_type]){
+			     $counter = 0;
+				 foreach($row as $key => $val){
+				      
+					  if($counter>=$end[$price_type]){
+						  unset($row[$key]);
+					  } 
+					  $counter++;
+				  }
+			  }	  /**/
+			 if($row['param_val']==0) array_push($row,'');
+			 else array_push($row,'<span class="deleteElementBtn" onclick="deleteRowFromTable(this);">&#215;</span>');
+			 //
+		     $tbl_row[$price_type][$count][] = $tr1.$td1.implode($td_td,$row).$td2.$tr2;
+			 $tbl_types[$price_type][$count]['cols_num'] = $end[$price_type];
+			 $tbl_row_ = $end[$price_type];
+			 //print_r($row);
 		}
 	}
 	else{
 	    echo 'no prices';
 		return;
 	}
-	
-	foreach($tbl_types as $type){
-	   echo 'тип прайса: '.$type;
-	   echo '<table>'.implode('',$tbl_row[$type]).'</table>';	
-	   echo '<br><br><br>';
+	//echo '<pre>'; print_r($tbl_types);echo '</pre>';
+	//echo '<pre>'; print_r($tbl_row);echo '</pre>';
+	foreach($tbl_types as $type => $data){
+
+	       $dop_row_data = array();
+		   for($i=0;$i<=$tbl_types[$type][$count]['cols_num'];$i++){
+			  if($i<2 || $i==$tbl_types[$type][$count]['cols_num']) $dop_row_data[$i]='<span></span>';
+			  else $dop_row_data[$i]='<span onclick="deleteColFromTable(this);" class="deleteElementBtn">&#215;</span>';
+		   }
+		   // print_r($dop_row_data);
+		   $dop_row = $tr1.$td1.implode($td_td,$dop_row_data).$td2.$tr2;
+		   echo 'тип прайса: '.$type;
+		   echo '<form method="POST">';
+		   echo '<table id="tbl'.$type.$key.'">'.implode('',$tbl_row[$type][$data['count']]).$dop_row.'</table>';	
+		   echo '<div>
+				  <span onclick="addRowsToTbl(\''.$type.'\','.$key.');">добавить</span><input size="1" id="rowsNum'.$type.$key.'" value="1">рядов
+				  &nbsp;&nbsp;
+				  <span onclick="addColsToTbl(\''.$type.'\','.$key.');">добавить</span><input size="1" id="colsNum'.$type.$key.'" value="1">колонок
+				 </div>';
+		   echo '<input type="hidden" name="dataBufferForSavingToBase" id="tblDataBuffer'.$type.$key.'" value="">';
+		   echo '<input type="button" onclick="priseManagerSendDataToBase(this.form,{\'type\':\'price\',\'bufferId\':\'tblDataBuffer'.$type.$key.'\',\'tblId\':\'tbl'.$type.$key.'\',\'price_type\':\''.$type.'\',\'print_type_id\':\''.$usluga_id.'\',\'count\':\''.$count.'\'});" value="сохранить">';
+		   echo '</form>';
+		   echo '<br><br><br>';
 	}
 	
 
--- skins/tpl/admin/price_manager/show.tpl
+++ skins/tpl/admin/price_manager/show.tpl
@@ -1,4 +1,110 @@
 <!-- begin skins/tpl/admin/price_manager/show.tpl --> 
+<script>
+function addRowsToTbl(type,key){
+   //alert('tbl'+type+key);
+   var tbl = document.getElementById('tbl'+type+key);
+   var rowsNum = parseInt(document.getElementById('rowsNum'+type+key).value);
+   rowsNum = (rowsNum>15)?15:rowsNum ;
+   addSomeRowsToTbl(tbl,rowsNum);
+  // alert(lastRow);
+}
+
+function addSomeRowsToTbl(tbl,num){
+   var rows = tbl.getElementsByTagName('TR');
+   var preLastRow = rows[rows.length-2];
+   var lastRow = rows[rows.length-1];
+
+   for(var i=0;i<num;i++){
+       var row = preLastRow.cloneNode(true);
+	   preLastRow.parentNode.insertBefore(row,lastRow);
+   }
+}
+
+function addColsToTbl(type,key){
+   //alert('tbl'+type+key);
+   var tbl = document.getElementById('tbl'+type+key);
+   var colsNum = parseInt(document.getElementById('colsNum'+type+key).value);
+   colsNum = (colsNum>15)?15:colsNum ;
+   addSomeColsToTbl(tbl,colsNum);
+  // alert(lastRow);
+}
+
+function addSomeColsToTbl(tbl,num){
+   //alert('tbl'+type+key);
+   var rows = tbl.getElementsByTagName('TR');
+  
+   
+   for(var i=0;i<rows.length;i++){
+       var cels = rows[i].getElementsByTagName('TD');
+	   var preLastCel = cels[cels.length-2];
+	   var lastCel = cels[cels.length-1];
+	   
+       for(var j=0;j<num;j++){
+	       var cel = preLastCel.cloneNode(true);
+           preLastCel.parentNode.insertBefore(cel,lastCel);
+		   
+	   }
+   }
+}
+
+function priseManagerSendDataToBase(form,data_obj){
+   //alert(data_obj.type);
+   if(data_obj.type=='price'){
+       var tbl = document.getElementById(data_obj.tblId);
+	   var rows = tbl.getElementsByTagName('TR');
+	   var dataForBuffer={};
+	   dataForBuffer.print_type_id = data_obj.print_type_id;
+	   dataForBuffer.price_type = data_obj.price_type;
+	   dataForBuffer.count = data_obj.count;
+	   dataForBuffer.tbl_data = [];
+	   
+       for(var i=0;i<rows.length-1;i++){
+	       var cels = rows[i].getElementsByTagName('TD');
+		   var celsData=[];
+		   for(var j=0;j<cels.length-1;j++){
+			   celsData[j] = cels[j].innerHTML;
+		   }
+
+		 
+		   dataForBuffer.tbl_data[i] = celsData;
+	   }
+	  
+
+	   //console.log(dataForBuffer);
+   }
+
+   document.getElementById(data_obj.bufferId).value =  JSON.stringify(dataForBuffer);
+   form.submit();
+   
+}
+function deleteRowFromTable(cell){
+   //alert('tbl'+type+key);
+   var tr = cell.parentNode.parentNode;
+   var tbl = tr.parentNode;
+   tbl.removeChild(tr);
+}
+function deleteColFromTable(cell){
+   var cell = cell.parentNode;
+   var tr = cell.parentNode;
+   var tbl = tr.parentNode;
+   var rows = tbl.getElementsByTagName('TR');
+  
+   var cels = tr.getElementsByTagName('TD');
+   for(var i=0;i<cels.length;i++){
+   ///alert(cels[i]);
+       if(cels[i]===cell) var cell_num = i;
+   }
+   //alert(cell_num);
+   if(cell_num){
+	   for(var i=0;i<rows.length;i++){
+		   var cels = rows[i].getElementsByTagName('TD');
+		   cels[cell_num].parentNode.removeChild(cels[cell_num]); 
+	   }
+   }
+}
+deleteColFromTable
+
+</script>
 <table class="mainWinTbl" border="1">
   <tr>
     <td width="150" >
